544fc7b20002a3f710a20183e9949913fd3e92e3
tlowther <proxy_phil@hotmail.com>
2012-03-03 19:30:43 +1100

    Code from Robot.md.

--- robot.rb
+++ robot.rb
@@ -1,38 +1,63 @@
 # ROBOT
 class Factory 
   
-  def initialize(@x, @y)
-    @@Factory = Array.new(@x){Array.new(@y){' '}}
-    i = 0
-    j = 0
-    @@Factory[0] = Array.new(@x){"*"}
-    @@Factory[@x-1] = Array.new(@x){"*"}
-    @@Factory = @@Factory.transpose
-    @@Factory[0] = Array.new(@y){"*"}
-    @@Factory[@y-1] = Array.new(@y){"*"}
-    @@Factory = @@Factory.transpose
-    @@Factory.each { |a| puts a.join(' ')}
+  $Factory = Array.new(){Array.new(){}}
+  
+  $Location = Array.new(3)
+  
+  $Orientation = Hash.new(0)
+  $Orientation["W"] = "^"
+  $Orientation["D"] = ">"
+  $Orientation["S"] = "V"
+  $Orientation["A"] = "<"
+  
+  $Moves = Hash.new(0)
+  $Moves["W"] = [0,-1]
+  $Moves["A"] = [-1,0]
+  $Moves["D"] = [1,0]
+  $Moves["S"] = [0,1]
+  
+  def initialize(x, y)
+    @x = x
+    @y = y
+    $Factory = Array.new(@x){Array.new(@y){' '}}
+    $Factory[0] = Array.new(@x){"*"}
+    $Factory[@x-1] = Array.new(@x){"*"}
+    $Factory = $Factory.transpose
+    $Factory[0] = Array.new(@y){"*"}
+    $Factory[@y-1] = Array.new(@y){"*"}
+    $Factory = $Factory.transpose
+    $Factory.each { |a| puts a.join(' ')}
+    $Moves.each { |a| puts a.join(' ')}
+    $Orientation.each { |a| puts a.join(' ')}
   end
 
-  def self.place_pkg(@x, @y, @d1, @d2, @type)
-    puts "Enter x and y coordinates > 0, please try again." if (@x < 1 or @y < 1) return end
-    puts "Enter package dimensions greater than zero, please try again." if (@d1 < 1) && (@d1 < 1) return end
-    else
-      @package = Array.new(@d1){Array.new(@d2){type}}
-      i= 0
-      j = 0
-      loop do
-        break unless @d1 > i
-        loop do 
-          break unless @d2 > j
-          @@factory[x+i][y+j] = @package[i][j] 
-          j+=1
-        end
-        i+=1
-        j = 0
-      end
+  def self.place_pkg(x, y, d1, d2, type)
+    @x = x
+    @y = y
+    @d1 = d1
+    @d2 = d2
+    @type = type
+    break if ((@x < 1) or (@y < 1))
+    puts "Enter x and y coordinates > 0, please try again."
     end
-    @@Factory.each { |a| puts a.join(' ')}
+    break if ((@d1 < 1) && (@d1 < 1))
+    puts "Enter package dimensions greater than zero, please try again."
+    end
+    @package = Array.new(@d1){Array.new(@d2){@type}}
+    i= 0
+    j = 0
+    loop do
+      break unless (@d1 > i)
+      loop do 
+        break unless (@d2 > j)
+        $Factory[x+i][y+j] = @package[i][j] 
+        j+=1
+      end
+      i+=1
+      j = 0
+    end
+  $Factory.each { |a| puts a.join(' ')}
   end
   
   def self.place_robot()
@@ -40,91 +65,41 @@ class Factory
     @x = STDIN.readline.chomp
     puts "Enter y coordinate:"
     @y = STDIN.readline.chomp
-    puts "Enter x and y coordinates > 0, please try again." if (@x < 1 or @y < 1) return end
-    puts "Enter orientation (N, E, S or W):"
+    puts "Enter x and y coordinates > 0, please try again." if (@x < 1 or @y < 1) break end
+    puts "Enter orientation (W=up,A=left,D=right,S=down):"
     @dir = STDIN.readline.chomp
-    @orientation = Hash.new(0)
-    puts "Invalid orientation, please try again." if @orientation[@dir] == 0 return end
-    @orientation["N"] = "^"
-    @orientation["E"] = ">"
-    @orientation["N"] = "V"
-    @orientation["N"] = "<"
-    
-    while @@factory[@x][@y] != " "
+    puts "Invalid orientation, please try again." if @@Orientation[@dir] == 0 break end
+    if (@@Factory[@x][@y] != " ")
       puts "Cannot place robot at chosen coordinates."
       puts "Enter x coordinate:"
       @x = STDIN.readline.chomp
       puts "Enter y coordinate:"
       @y = STDIN.readline.chomp
-      puts "Enter x and y coordinates > 0, please try again." if (@x < 1 or @y < 1) return end
-      puts "Enter direction (N, E, S or W):"
+      puts "Enter direction (W=up,A=left,D=right,S=down):"
       @dir = STDIN.readline.chomp
-      puts "Invalid orientation, please try again." if @orientation[@dir] == 0 return end
-    end
-      
-    @@factory[@x][@y] = @orientation[@dir]    
-    
-    end
-
-  def move_robot
-    while l = STDIN.readline.chomp
-    puts "got input '#{l}'"
-  end
-  
-  
-  class Book
-
-    def self.add_to_shelf(book)
-      @bookshelf = [] if @bookshelf == nil
-      @bookshelf << book
-      @bookshelf
-    end
-
-    def self.bookshelf
-      @bookshelf
-    end
-
-    def initialize(title, page_count)
-      @title = title
-      @page_count = page_count
-      Book.add_to_shelf(self)
-    end
-
-    attr_reader :title
-
-    def long?
-      if @page_count > 100
-        "yes"
-      else
-        "no"
+      if (@x < 1 or @y < 1) puts "Enter x and y coordinates > 0, please try again."
+      end
+      if (@@Orientation[@dir] == 0) puts "Invalid orientation, please try again."
       end
     end
+    
+      
+    @@Factory[@x][@y] = @@Orientation[@dir]
+    @@Location = [@x,@y,@@Orientation[@dir]]
+  end
 
+  def self.move_robot()
+    while @l = STDIN.readline.chomp
+      puts "Enter movement command (W=up,A=left,D=right,S=down):"
+      puts "Invalid input, enter movement command (W=up,A=left,D=right,S=down):" if @@Moves[@l] == 0 return
+      moves = @@Moves[@l]
+      if @@Factory[@@Location[0] + moves[1], @@Location[1] + moves[1]] = ' ' then
+        @@Factory[@@Location[0], @@Location[1]] = ' '
+        @@Factory[@@Location[0] + moves[1], @@Location[1] + moves[1]] = @@Orientation[@l]
+        else puts "Cannot move, try alternative direction." 
+      end
+      @@Factory.each { |a| puts a.join(' ')}
+    end
   end
-  
-x = y = d1 = d2 = 2
-factory = [
-  "****************************************",
-  "*                                      *",
-  "*                                      *",
-  "*                                      *",
-  "*                                      *",
-  "*                                      *",
-  "*                                      *",
-  "*                                      *",
-  "*                                      *",
-  "****************************************"
-]
-package = Array.new(d1){Array.new(d2){'A'}}
-i= 0
-j = 0
-loop do
-  break unless d1 > i
-  loop do 
-    break unless d2 > j
-    factory[x+i][y+j] = package[i][j] 
-    j+=1
-  end
-  i+=1
-  j = 0
 end
+  
