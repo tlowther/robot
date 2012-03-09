8a2c814478b281304fb520ab8ed266a3f4384b6b
tlowther <proxy_phil@hotmail.com>
2012-03-10 00:25:12 +1100

    Floor plan initialisation modified, package class added

--- robot.rb
+++ robot.rb
@@ -1,6 +1,6 @@
 # ROBOT
-class Factory 
-
+class Factory
+  
   ORIENTATION = { 
     "N" => "^",
     "E" => ">",
@@ -19,28 +19,36 @@ class Factory
     @x = x
     @y = y
     
-    # STILL NEED TO SWAP x AND Y
-    @factory = Array.new(@x) { Array.new(@y){' '} }
-    @factory[0] = Array.new(@x) { "*" }
-    @factory[@x-1] = Array.new(@x) { "*" }
-    @factory = @factory.transpose # only works on square areas
-    @factory[0] = Array.new(@y){ "*" }
-    @factory[@y-1] = Array.new(@y){ "*" }
-    @factory = @factory.transpose # only works on square areas
-  end
+    bottom_row = top_row = Array.new(@x) { "*" }
+    internal_row = Array.new(@x) { " " }
+    internal_row[0] = "*"
+    internal_row[-1]= "*"
+    
+    @floorplan = Array.new(@y) { Array.new(@x){' '} }
+    @floorplan[0] =  top_row #inserts 'walls' top row of floor
+    @floorplan[-1] = bottom_row #inserts 'walls' bottom row of floor
 
+    (1..(@y-2)).each do |i| #inserts remaining internal rows
+      @floorplan[i] = internal_row.dup
+    end
+  end
+  
   def place_pkg(x, y, d1, d2, type)
-    (x..(x+d1-1)).each do |i|
-      (y..(y+d2-1)).each do |j|
-        @factory[i][j] = type
+    pkg = Package.new(x, y, d1, d2, type)
+    (y..(y+d2-1)).each do |i|
+      (x..(x+d1-1)).each do |j|
+        @floorplan[i][j] = pkg
       end
     end
     self
   end
   
-  def show_factory
-    @factory.each { |a| puts a.join(' ') }
-    self
+  def to_s
+    nil
+  end
+  def show
+    @floorplan.each { |a| puts a.join(' ') }
+    nil
   end
   
   def place_robot
@@ -53,12 +61,12 @@ class Factory
       return nil
     end
     puts "Enter orientation (N, E, S, W):"
-    dir = STDIN.readline.chomp
+    dir = STDIN.readline.chomp.upcase
     if ORIENTATION[dir].nil?
       puts "Invalid orientation, please try again."
       return nil
     end
-    @factory[x][y] = ORIENTATION[dir]
+    @floorplan[y][x] = ORIENTATION[dir]
     @location = [x,y,ORIENTATION[dir]]
     self
   end
@@ -66,14 +74,14 @@ class Factory
   def move_robot_forever
     while true do
       move_robot
-      show_factory
+      show
     end
     puts "Execution never gets here!"
   end
   
   def move_robot
     puts "Enter movement command (N, E, S, W):"
-    l = STDIN.readline.chomp
+    l = STDIN.readline.chomp.upcase
     unless MOVES.keys.include?(l)
       puts "Invalid input, enter movement command (N, E, S, W)!"
       return nil
@@ -83,36 +91,47 @@ class Factory
     new_x = @location[0] + this_move[0]
     new_y = @location[1] + this_move[1]
 
-    if @factory[new_x][new_y] == ' '
-      @factory[new_x][new_y] = ORIENTATION[l]
-      @factory[@location[0]][@location[1]] = ' '
+    if @floorplan[new_y][new_x] == ' '
+      @floorplan[new_y][new_x] = ORIENTATION[l]
+      @floorplan[@location[1]][@location[0]] = ' '
       @location = [new_x, new_y, ORIENTATION[l]]
-    else
+    
+    elsif @floorplan[new_y][new_x] == '*'
       puts "Cannot move, try alternative direction (this_move => #{this_move})." 
+    
+    else 
+      print "Cannot move to [#{new_x}, #{new_y}], "
+      print "package at #{@floorplan[new_y][new_x].loc} " 
+      puts "of dimensions #{@floorplan[new_y][new_x].dims}"
+    
     end
     self
   end
   
 end 
 
-# class Floor
-#   
-#   def initialise(x,y)
-#     @plan = Array.new(@y) { Array.new(@x){' '} }
-#   end
-#   
-#   def show
-#     
-#   end
-#   
-#   def get(x,y)
-#     @plan[x][y]
-#   end
-# 
-#   def set(x,y,value)
-#     @plan[x][y] = value
-#   end
-#   
-# end
+class Package
+  
+  def initialize(x, y, d1, d2, type)
+    @x = x
+    @y = y
+    @d1 = d1
+    @d2 = d2
+    @type = type
+  end
+  
+  def to_s
+    @type
+  end
+  
+  def dims
+    [@d1, @d2]
+  end
+
+  def loc
+    [@x, @y]
+  end  
+
+end
 
 
